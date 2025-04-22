97fd802aaeaa6b3647f7353d1a8b4f13357dbd54
maria_reznyk <maria.reznyk@gmail.com>
2025-04-22 22:44:42 +0100

    task 3 complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
+import PageHeader from "./components/PageHeader/PageHeader";
 import Section from "./components/Section/Section";
 import Container from "./components/Container/Container";
 import Profile from "./components/Profile/Profile";
 import FriendList from "./components/FriendList/FriendList";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
 import userData from "./data/userData.json";
 import friends from "./data/friends.json";
+import transactions from './data/transactions.json';
 
 
 function App() {
     return (<Section>
+        <Container>
+            <PageHeader>Homework #1</PageHeader>
+        </Container>
+
         <Container>
             <Profile name={userData.username}
             tag={userData.tag}
@@ -18,6 +25,10 @@ function App() {
 
         <Container>
             <FriendList friends={friends}/>
+        </Container>
+
+        <Container>
+            <TransactionHistory items={transactions}/>
         </Container>
     </Section>);
 }
