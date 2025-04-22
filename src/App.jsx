5fa1b721cca724474ee34ec428d6692553aa219c
maria_reznyk <maria.reznyk@gmail.com>
2025-04-22 15:38:09 +0100

    task 2 complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import Section from "./components/Section/Section";
 import Container from "./components/Container/Container";
-import userData from "./data/userData.json";
 import Profile from "./components/Profile/Profile";
+import FriendList from "./components/FriendList/FriendList";
+import userData from "./data/userData.json";
+import friends from "./data/friends.json";
+
 
 function App() {
     return (<Section>
         <Container>
-        <Profile name={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        image={userData.avatar}
-        stats={userData.stats}/>
+            <Profile name={userData.username}
+            tag={userData.tag}
+            location={userData.location}
+            image={userData.avatar}
+            stats={userData.stats}/>
+        </Container>
+
+        <Container>
+            <FriendList friends={friends}/>
         </Container>
     </Section>);
 }
